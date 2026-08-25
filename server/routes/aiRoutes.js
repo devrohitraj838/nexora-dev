814d1d62a457a4c59e43e95187f679f96e90ce9e
devrohitraj838 <raj826444@gmail.com>
2026-08-25 18:59:19 +0530

    aiRoutes issue resolve

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { getDailyInsight } = require("../controllers/aiController");
 const { protect } = require("../middleware/authMiddleware");
 
-// Using POST so we can securely send the stats in the request body
-router.post("/insight", protect, getDailyInsight);
+router.post("/insights", protect, getDailyInsight);
 
 module.exports = router;
